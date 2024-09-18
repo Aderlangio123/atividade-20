505701aed50dbdb05823103000bebdcfa14bc73c
Aderlangio123 <faderlangio123@gmail.com>
2024-09-18 16:04:47 -0300

    atividade 20 - exibir numeros pares de 1 a 100l

--- atividade20.py
+++ atividade20.py
@@ -1,6 +1,9 @@
 # Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
 
+valor = 0
+
 for n in range(1, 101):
 
     if n %2 ==0:
-        print(n)
+        valor = valor + n
+print(valor)
